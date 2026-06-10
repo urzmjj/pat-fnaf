28c0dc9917f689e8b5e041bfe53bcd978782e2fa
urzmjj <120228667+urzmjj@users.noreply.github.com>
2026-06-10 18:05:16 +1000

    added quit button functionality

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -14,3 +14,8 @@ func _process(delta: float) -> void:
 
 func _on_custom_night_button_pressed() -> void:
 	CustomNight.show()
+
+
+func _on_quit_button_pressed() -> void:
+	# get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
