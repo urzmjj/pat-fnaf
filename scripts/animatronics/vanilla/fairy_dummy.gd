6c0527a01bf8bcc2195ef95d67a581267fc0425b
urzmjj <120228667+urzmjj@users.noreply.github.com>
2026-08-06 20:29:35 +1000

    added more filter

--- scripts/animatronics/vanilla/fairy_dummy.gd
+++ scripts/animatronics/vanilla/fairy_dummy.gd
@@ -1,8 +1,11 @@
 class_name FairyDummy
 extends Animatronic
 
+var door: Constants.Door
+
 func reset():
 	progress = 0
+	door = Constants.Door.values().pick_random()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
